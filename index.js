b6b70e4d049b4607c3086dece227d9d72f94c0d8
HerodotThukydides <jptebbe@hotmail.com>
2017-02-06 11:44:02 +0100

    Comments

--- index.js
+++ index.js
@@ -11,8 +11,14 @@ restService.use(bodyParser.urlencoded({
 
 restService.use(bodyParser.json());
 
+// Creating an API which accepts the parameter "echo" and makes a post
 restService.post('/echo', function(req, res) {
+  // Next to the structure of the answer there is also:
+  // req.body.result.parameters.echoText
+  // "echoText" is the actual content which this API is expecting - if it is another content there will be message "Seems like there is a problem.."
     var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.echoText ? req.body.result.parameters.echoText : "Seems like some problem. Speak again."
+   
+    // this is the response, which is exactly the same as the incoming message
     return res.json({
         speech: speech,
         displayText: speech,
